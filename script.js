cc82fd8364f0442e1fe25be18dd6f0b678dc7fc0
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-02 15:45:44 +0530

    String Declearation
    Single quotes ,Double quotes and backtricks as well.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//String Data-Type
+let singleQuote = 'Hello-World';
+let doubleQuote = "Hello-World";
+let backTricks  = `Hello`;
+
+let name = "Yashraj";
+//Single and double quotes
+alert("My name is : " + name);
+
+alert(`My name is ${name}`);
