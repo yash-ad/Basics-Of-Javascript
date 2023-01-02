54b8da66911b8a9478417ff98110ca101a7d1957
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-02 16:15:00 +0530

    Update script.js
    Number Data-Type

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ let name = "Yashraj";
 //Single and double quotes
 alert("My name is : " + name);
 
+
+ //BY using backtrick for embed variable and for expression,This is a numeric property.
 alert(`My name is ${name}`);
+
+//And for expression
+alert(`And the expression is ${1 + 2 }`);
+
+
+//Number Data-Type
+
+let number = 123456;
+
+let One = 10;
+let Two = 20;
+
+alert(`The expresssion is ${ One + Two }`);
