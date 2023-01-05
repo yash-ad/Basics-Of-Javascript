2555cbd0ff8b418707eb1d6f818ea21128bd14a2
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-05 10:15:57 +0530

    Decleared a variables by using var , let and const keyword

--- variables.js
+++ variables.js
@@ -1,3 +1,10 @@
 var   name = "Yashraj";
-let   naav = "Yashraj";
-const naam = "Yashraj";
+let   Name = "Yashraj";
+const naMe = "Yashraj";
+alert("My name is :" + name);
+alert("My name is :" + Name);
+alert("My name is :" + naMe);
+
+//Javascript is a case Sensitive-Language
+//The variables "name , Name , naMe are three different values"
+//All javscript identifiers are case sensitive.
