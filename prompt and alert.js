477cf551f6493e152c0eb15a5f124d89ae3dbd37
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-06 13:36:27 +0530

    User input and output

---
+++ prompt and alert.js
@@ -0,0 +1,5 @@
+var tweet = prompt("Compose your tweet");
+
+var tweetCount = tweet.length;
+
+alert(" Your tweet character written is " + tweetCount + " & " + " The remaining characters is " + (200-tweetCount));
