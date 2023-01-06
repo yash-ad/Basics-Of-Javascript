6ddae6c0e7f5a4c33f6d54455e2bed7a751bda8b
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-06 09:19:26 +0530

    codung exercise test

--- variables.js
+++ variables.js
@@ -8,3 +8,18 @@ alert("My name is :" + naMe);
 //Javascript is a case Sensitive-Language
 //The variables "name , Name , naMe are three different values"
 //All javscript identifiers are case sensitive.
+
+
+//Lets do some variables test exercise:-
+
+var a = "3";
+var b = "8";
+//Lets say var a is a sand bucket and var b is water bucket so we need to exchange these materials,so all i have to do is i want an another bucket to fill these materials in it.
+ 
+ var c = a;
+ var a = b;
+ var b = c;
+
+
+console.log("The value of a is :" + a );
+console.log("The value of b is :" + b );
