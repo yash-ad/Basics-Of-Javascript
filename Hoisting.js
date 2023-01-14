533136a9886935053cf00b517de6081fb694d337
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-01-14 10:12:26 +0530

    added file Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,68 @@
+//Hoist means to raise or lift something in simple words:-
+
+//1.What is hoisting in javascript? //IMP in Js
+//Hoisting is javascripts by default behavior of moving decalrations at the top.
+
+//Lets take an example 1.
+a = "Yash";
+var a;
+console.log(a);
+
+
+//Lets take an example 2.
+
+var b;
+b = "Dandnaik";
+console.log(b);
+
+//Hoisting is javascripts by default behavior of moving all declarations at the top of the current scope.
+
+
+
+//Let and const Keywords:-
+
+
+
+//Varaiables defined with the let and const keywords are hoisted to the top of the block, however it cannot be intialized.
+ 
+
+//Let keyword example:-
+lastName = "Dandnaik";
+ let lastName;
+ console.log(lastName); //It will caught an error because before it is declared.
+
+//Const keyword example:-
+myName = "Yash";
+const myName = "Yash";
+console.log(myName);//It will caught an error beacuse must be intialized beforeits declaration
+
+//Javascript intializations are not hoisted:-
+//Javascript only hoists declarations not intializations.
+var x = 2; //Intialize X to the assigning value 2.
+var y = 3; //Intialize Y to teh assigning value 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
