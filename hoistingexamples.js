165e244ec2196dd85268a0749669876e4e1c2c41
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-02-06 11:22:24 +0530

    Hoisting examples for Js Interview

--- hoistingexamples.js
+++ hoistingexamples.js
@@ -15,7 +15,7 @@ var b = "Dandnaik"; //Dandnaik in a memory before executing
 console.log(b);// Value = Dandnaik
 
 //Function declaration example.
-myName()
+myName() //The value is "My name is Yash ";
 function myName()
 {
 console.log("My name is Yash ");
@@ -25,7 +25,7 @@ console.log("My name is Yash ");
 //Functon expression example
 //nature(); //Undefined it will caught an error because it cannot access before intialization.
 
-//const nature = functon()
+//const nature = functon universe()
 {
 
     //console.log("Earth");
@@ -33,9 +33,35 @@ console.log("My name is Yash ");
 //nature(); //It will also give an error because nature is not a function.
 
 //Examples with let and const:-
-debugger;
-//Compilation phase variables and fucntions allocate memory.
+//Compilation phase variables and functions allocate memory.
 //Interpretation phase execution of the code the value to a stored memory.
 console.log(c) // this will caught an error with let and const before intialization we cannot access them.
 let c = "Cat"; // In a compilation phase C varaible allocated to memory by default Undefined before execution.
 console.log(c);// The value of C is Cat.when program starts an execution phase or process.
+
+//Hoisting examples for JS interview  :-
+
+var a = 20; //Compilation phase undefined
+//Executon phase 20 assigned to a;
+var a; // already declared a which is an undefined in a memory
+//this is not an execution.
+console.log(a); //Not execution in compile phase
+//IN an execution phase the value of a ina stored memory is 20 so the value of a is 20;
+
+var v = 5050; //Undefined stored in a memory due to a compilation step.
+//In an execution step variable v undefined value is assigned to 5050 stored in a memory.
+var v;//already declared v in a memory which is an undefined. and not reassigning in an execution step.
+console.log(v);//Not an execution in a compile step.
+//In an executon step the value of variable v is 5050;
+
+var d = "Dandnaik";
+console.log(d); //Dandnaik
+
+//Scope and hoisting examples
+function fn(){
+console.log(d); //Undefined
+var d = 50; //Undefined in compile step
+console.log(d); //50
+
+}
+fn();
