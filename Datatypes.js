796c5724091f9dea320fdf85e3b1322afc11d8e4
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-02-20 14:33:32 +0530

    Examples of datatypes

--- Datatypes.js
+++ Datatypes.js
@@ -4,3 +4,20 @@ console.log("The type of a is", typeof(myName));  //Type of is a function and to
 
 let myBirth_date = 31; //Number datatype
 console.log("My Birthdate is",myBirth_date,"st March");
+
+let activeLifestyle = true; //Boolean Data-type
+let sedentaryLifestyle = false;
+console.log("The activelifestyle is ",activeLifestyle);
+console.log("The sedentarylifestyle is ",sedentaryLifestyle);
+
+let absenceValue = null; //Null data-type
+console.log("The absence of value is ",absenceValue);
+
+let UD; //Undefined data-type
+console.log("The value of UD is ",UD);
+
+let bigInt = 9552739727; //BigInt datatype
+console.log("The value of BigInt is", bigInt ,"Which is a bunch of numbers.");
+
+let symbol = "Id"; //Symbol Data-type
+console.log("The value of a symbol is",symbol," , Which is an unique identifier.");
