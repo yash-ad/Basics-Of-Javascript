eca1ad1e3da5861cd7e77fdb4bae7ce7426a9c39
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-02-20 13:17:28 +0530

    datatypes examples

---
+++ Datatypes.js
@@ -0,0 +1,6 @@
+let myName = "Yashraj"; //String datatype;
+console.log("Hello I'm ",myName,", pleasure to meet you !");
+console.log("The type of a is", typeof(myName));  //Type of is a function and to identify what type of datatype is.
+
+let myBirth_date = 31; //Number datatype
+console.log("My Birthdate is",myBirth_date,"st March");
